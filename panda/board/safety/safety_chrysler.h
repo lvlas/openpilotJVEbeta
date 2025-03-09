feb8136912965b60a8f1c3e115b05dc44f10fde9
lvlas <106373597+lvlas@users.noreply.github.com>
2025-03-09 09:03:45 +0100

    Update safety_chrysler.h torque /4

--- panda/board/safety/safety_chrysler.h
+++ panda/board/safety/safety_chrysler.h
@@ -204,7 +204,7 @@ static void chrysler_rx_hook(const CANPacket_t *to_push) {
   // Measured EPS torque
   if ((bus == 0) && (addr == chrysler_addrs->EPS_2)) {
     int torque_meas_new = ((GET_BYTE(to_push, 4) & 0x7U) << 8) + GET_BYTE(to_push, 5) - 1024U;
-    update_sample(&torque_meas, torque_meas_new);
+    update_sample(&torque_meas, torque_meas_new/4);
   }
 
   if ((bus == 0) && (addr == chrysler_addrs->GEAR)) {
