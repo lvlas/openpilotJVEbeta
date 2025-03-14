c4238b701518d8765308df47158fc595c11a8181
lvlas <106373597+lvlas@users.noreply.github.com>
2025-03-14 20:32:05 +0100

    Update interface.py

--- selfdrive/car/chrysler/interface.py
+++ selfdrive/car/chrysler/interface.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3 aaa
 from cereal import car
 from panda import Panda
 from openpilot.selfdrive.car import get_safety_config
@@ -29,6 +29,7 @@ class CarInterface(CarInterfaceBase):
         maxAccel = cachedParams.get_float('jvePilot.settings.accEco.longAccelLevel2', 1000)
       else:
         maxAccel = 2
+
     return maxAccel
 
   @staticmethod
@@ -36,10 +37,12 @@ class CarInterface(CarInterfaceBase):
     ret.carName = "chrysler"
     ret.dashcamOnly = candidate in RAM_HD
 
+    # radar parsing needs some work, see https://github.com/commaai/openpilot/issues/26842
     ret.radarUnavailable = DBC[candidate]['radar'] is None
     ret.steerActuatorDelay = 0.1
     ret.steerLimitTimer = 0.4
 
+    # safety config
     ret.safetyConfigs = [get_safety_config(car.CarParams.SafetyModel.chrysler)]
     if candidate in RAM_HD:
       ret.safetyConfigs[0].safetyParam |= Panda.FLAG_CHRYSLER_RAM_HD
@@ -50,11 +53,13 @@ class CarInterface(CarInterfaceBase):
 
     CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
     if candidate not in RAM_CARS:
+      # Newer FW versions standard on the following platforms, or flashed by a dealer onto older platforms have a higher minimum steering speed.
       new_eps_platform = candidate in (CAR.CHRYSLER_PACIFICA_2019_HYBRID, CAR.CHRYSLER_PACIFICA_2020, CAR.JEEP_GRAND_CHEROKEE_2019, CAR.DODGE_DURANGO)
       new_eps_firmware = any(fw.ecu == 'eps' and fw.fwVersion[:4] >= b"6841" for fw in car_fw)
       if new_eps_platform or new_eps_firmware:
         ret.flags |= ChryslerFlags.HIGHER_MIN_STEERING_SPEED.value
 
+    # Chrysler
     if candidate in (CAR.CHRYSLER_PACIFICA_2017_HYBRID, CAR.CHRYSLER_PACIFICA_2018, CAR.CHRYSLER_PACIFICA_2018_HYBRID, \
                      CAR.CHRYSLER_PACIFICA_2019_HYBRID, CAR.CHRYSLER_PACIFICA_2020, CAR.DODGE_DURANGO):
       if params.get_bool("jvePilot.settings.steer.pid"):
@@ -62,20 +67,42 @@ class CarInterface(CarInterfaceBase):
         ret.lateralTuning.pid.kpBP, ret.lateralTuning.pid.kiBP = [[9., 20.], [9., 20.]]
         ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.15, 0.30], [0.03, 0.05]]
         ret.lateralTuning.pid.kf = 0.00006
-      ret.experimentalLongitudinalAvailable = False
 
+      ret.experimentalLongitudinalAvailable = False # candidate not in HYBRID_CARS
+
+    # Jeep
     elif candidate in (CAR.JEEP_GRAND_CHEROKEE, CAR.JEEP_GRAND_CHEROKEE_2019):
-      ret.steerActuatorDelay = 0.2
+      ret.steerActuatorDelay = 0.08 #nebylo nic
+#
+      #ret.lateralTuning.init('pid')
+      #ret.lateralTuning.pid.kpBP, ret.lateralTuning.pid.kiBP = [[9., 20.], [9., 20.]]
+      #ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.15, 0.30], [0.03, 0.05]]
+      #ret.lateralTuning.pid.kf = 0.00006
+
+      #ret.lateralTuning.init('pid')
+      #ret.lateralTuning.pid.kiBP = [0.0]
+      #ret.lateralTuning.pid.kpBP = [0.0,10.0,20.0]
+      #ret.lateralTuning.pid.kpV = [0.019,0.018,0.016]
+      #ret.lateralTuning.pid.kiV = [0.0004]
+      #ret.lateralTuning.pid.kf = 0.00006      
+      #ret.minSteerSpeed = 0.0
       ret.lateralTuning.init('pid')
-      ret.lateralTuning.pid.kpBP, ret.lateralTuning.pid.kiBP = [[9., 20.], [9., 20.]]
-      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.15, 0.30], [0.03, 0.05]]
-      ret.lateralTuning.pid.kf = 0.00006
+      ret.lateralTuning.pid.kiBP = [0.0]
+      ret.lateralTuning.pid.kpBP = [0.0,3.0,10.0]
+      ret.lateralTuning.pid.kpV = [0.019,0.017,0.015]
+      ret.lateralTuning.pid.kiV = [0.0004]
+      ret.lateralTuning.pid.kf = 0.00006      
+      ret.minSteerSpeed = 0.0
       ret.enableBsm = True
       ret.experimentalLongitudinalAvailable = True
 
+      
+      
+    # Ram
     elif candidate == CAR.RAM_1500_5TH_GEN:
       ret.steerActuatorDelay = 0.2
       ret.wheelbase = 3.88
+      # Older EPS FW allow steer to zero
       if any(fw.ecu == 'eps' and b"68" < fw.fwVersion[:4] <= b"6831" for fw in car_fw):
         ret.minSteerSpeed = 0.
 
@@ -87,14 +114,17 @@ class CarInterface(CarInterfaceBase):
       raise ValueError(f"Unsupported car: {candidate}")
 
     if ret.flags & ChryslerFlags.HIGHER_MIN_STEERING_SPEED:
-      ret.minSteerSpeed = 17.5
+      # TODO: allow these cars to steer down to 13 m/s if already engaged.
+      # TODO: Durango 2020 may be able to steer to zero once above 38 kph
+      ret.minSteerSpeed = 17.5  # m/s 17 on the way up, 13 on the way down once engaged.
 
     ret.centerToFront = ret.wheelbase * 0.44
     ret.enableBsm |= 720 in fingerprint[0]
 
-    ret.openpilotLongitudinalControl = True
-    ret.pcmCruiseSpeed = False
+    ret.openpilotLongitudinalControl = True  # kind of...
+    ret.pcmCruiseSpeed = False  # Let jvePilot control the pcm cruise speed
 
+    # Autodetect WP
     if (0x4FF in fingerprint[0]) or params.get_bool("jvePilot.settings.steer.noMinimum"):
       params.put_bool_nonblocking("jvePilot.settings.steer.noMinimum", True)
       ret.minSteerSpeed = -0.1
@@ -104,8 +134,12 @@ class CarInterface(CarInterfaceBase):
   def _update(self, c):
     ret = self.CS.update(self.cp, self.cp_cam)
 
+    #ret.buttonEvents = create_button_events(self.CS.distance_button, self.CS.prev_distance_button, {1: ButtonType.gapAdjustCruise})
+
+    # events
     events = self.create_common_events(ret, extra_gears=[car.CarState.GearShifter.low])
 
+    # Low speed steer alert hysteresis logic
     if self.CP.minSteerSpeed > 0. and ret.vEgo < (self.CP.minSteerSpeed + 0.5):
       self.low_speed_alert = True
     elif ret.vEgo > (self.CP.minSteerSpeed + 1.):
