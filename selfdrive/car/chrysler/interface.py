c6568fc60dbf1826c879f73aa438db114ee4dd9c
lvlas <106373597+lvlas@users.noreply.github.com>
2025-03-14 20:22:28 +0100

    Update interface.py úplně last known

--- selfdrive/car/chrysler/interface.py
+++ selfdrive/car/chrysler/interface.py
@@ -65,11 +65,11 @@ class CarInterface(CarInterfaceBase):
       ret.experimentalLongitudinalAvailable = False
 
     elif candidate in (CAR.JEEP_GRAND_CHEROKEE, CAR.JEEP_GRAND_CHEROKEE_2019):
-      ret.steerActuatorDelay = 0.25
+      ret.steerActuatorDelay = 0.2
       ret.lateralTuning.init('pid')
       ret.lateralTuning.pid.kpBP, ret.lateralTuning.pid.kiBP = [[9., 20.], [9., 20.]]
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.15, 0.30], [0.03, 0.05]]
-      ret.lateralTuning.pid.kf = 0.00005
+      ret.lateralTuning.pid.kf = 0.00006
       ret.enableBsm = True
       ret.experimentalLongitudinalAvailable = True
 
